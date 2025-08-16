2965755f9baebe5174af560d0017dfcb21872700
Mathias <mathiasbohndk@gmail.com>
2025-08-16 10:46:36 -0400

    Tests passed

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
+require('datejs');
 
+// Function to combine users
+function combineUsers(...args) {
+  const combinedObject = { users: [] };
 
+  // Loop through args
+  for (let i = 0; i < args.length; i++) {
+    const currentArray = args[i];
+    combinedObject.users = [...combinedObject.users, ...currentArray];
+  }
+
+  // Get today's date using datejs
+  const today = new Date();
+  combinedObject.merge_date = today.toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
